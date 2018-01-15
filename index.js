d9f7408b64956fe0347c0405dcf377167c9e614c
Ray <ray@optymyze.com>
2018-01-15 20:00:53 +0530

    moved the files.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './navigation-screens/app';
+import App from './src/navigation-screens/app';
+import FlatListDemo from "./src/FlatListDemo";
 
 AppRegistry.registerComponent('ReactNativeToDoApp', () => App);

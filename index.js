5431c42fb96caa69b9b7b02f5036cefadf870552
Ray <ray@optymyze.com>
2018-01-15 20:14:46 +0530

    reverted to last working Navigation App

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './src/navigation-screens/app';
-import FlatListDemo from "./src/FlatListDemo";
 
 AppRegistry.registerComponent('ReactNativeToDoApp', () => App);

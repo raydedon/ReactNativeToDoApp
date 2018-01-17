8ec7a7d0a6a9429f338aacec1d26e4dd6fb0704e
Ray <ray@optymyze.com>
2018-01-17 19:01:26 +0530

    implemented flatlist

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './src/navigation-screens/app';
+import FlatListDemo from "./src/FlatListDemo";
+import ListViewTasks from "./src/views/list-view-tasks";
 
-AppRegistry.registerComponent('ReactNativeToDoApp', () => App);
+
+AppRegistry.registerComponent('ReactNativeToDoApp', () => ListViewTasks);
---
+++ src/views/list-view-tasks.js
@@ -0,0 +1,170 @@
+import React, { Component } from 'react';
+import {
+    View,
+    ListView,
+    FlatList,
+    StyleSheet,
+    Text,
+    Image,
+    TextInput,
+    ActivityIndicator,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    input: {
+        height: 40,
+        flex: 1,
+        paddingHorizontal: 8,
+        fontSize: 15,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 2,
+    },
+    listItemContainer: {
+        flex: 1,
+        padding: 12,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    text: {
+        marginLeft: 12,
+        fontSize: 16,
+    },
+    photo: {
+        height: 40,
+        width: 40,
+        borderRadius: 20,
+    },
+});
+
+export default class ListViewTasks extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: false,
+            data: [],
+            page: 1,
+            seed: 1,
+            error: null,
+            refreshing: false
+        };
+    }
+    componentDidMount() {
+        this.makeRemoteRequest();
+    }
+
+    makeRemoteRequest = () => {
+        const { page, seed } = this.state;
+        const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
+        this.setState({ loading: true });
+
+        fetch(url)
+            .then(res => res.json())
+            .then(res => {
+                this.setState({
+                    data: page === 1 ? res.results : [...this.state.data, ...res.results],
+                    error: res.error || null,
+                    loading: false,
+                    refreshing: false
+                });
+            })
+            .catch(error => {
+                this.setState({ error, loading: false });
+            });
+    };
+
+    handleRefresh = () => {
+        this.setState(
+            {
+                page: 1,
+                seed: this.state.seed + 1,
+                refreshing: true
+            },
+            () => {
+                this.makeRemoteRequest();
+            }
+        );
+    };
+
+    handleLoadMore = () => {
+        this.setState(
+            {
+                page: this.state.page + 1
+            },
+            () => {
+                this.makeRemoteRequest();
+            }
+        );
+    };
+
+    renderSeparator = () => {
+        return (
+            <View
+                style={{
+                    height: 1,
+                    width: "86%",
+                    backgroundColor: "#CED0CE",
+                    marginLeft: "14%"
+                }}
+            />
+        );
+    };
+
+    renderHeader = () => {
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Search..."
+                    onChangeText={(text) => console.log('searching for ', text)}
+                />
+            </View>
+        );
+    };
+
+    renderFooter = () => {
+        if (!this.state.loading) return null;
+
+        return (
+            <View
+                style={{
+                    paddingVertical: 20,
+                    borderTopWidth: 1,
+                    borderColor: "#CED0CE"
+                }}
+            >
+                <ActivityIndicator animating size="large" />
+            </View>
+        );
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <FlatList
+                    data={this.state.data}
+                    renderItem={({ item }) => (
+                        <View style={styles.listItemContainer}>
+                            <Image source={{ uri: item.picture.large}} style={styles.photo} />
+                            <Text style={styles.text}>{`${item.name.first} ${item.name.last}`}</Text>
+                        </View>
+                        )}
+                    keyExtractor={item => item.email}
+                    ItemSeparatorComponent={this.renderSeparator}
+                    ListHeaderComponent={this.renderHeader}
+                    ListFooterComponent={this.renderFooter}
+                    onRefresh={this.handleRefresh}
+                    refreshing={this.state.refreshing}
+                    onEndReached={this.handleLoadMore}
+                    onEndReachedThreshold={50}
+                />
+            </View>
+        );
+    }
+}
